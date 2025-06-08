9e3c9a2ccdf37b0e592bf8944462450a8d747f85
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-08 09:26:14 +0300

    add 03_01_Conditional_Statements_Advanced_Lab 00_for_testing.py

---
+++ 03_Conditional_Statements_Advanced/03_01_Conditional_Statements_Advanced_Lab/00_for_testing.py
@@ -0,0 +1,126 @@
+# # bgn_to_usd = 0.75
+#
+# bgn = float(input('Въведи БГ левове: '))
+# currency = input('Въведи в коя валута?: (eur/usd) ')
+#
+# if currency == 'usd':  # 'USD' == 'usd'  => False
+#     usd = bgn * 0.75
+#     print(f'В долари: {usd:.2f}!')
+# elif currency == 'eur':
+#     eur = bgn * 0.85
+#     print(f'В евро: {eur:.2f}!')
+
+# result = 123456 % 100
+# to_compare_with = 56
+#
+# end_result = result == to_compare_with
+# print(end_result)
+
+# num = 100
+#
+# if num > 500:
+#     print('it is greater than 50')
+#     if num > 75:
+#         print('it is greater than 75')
+#
+# print('inside outer if, but not inside inner if')
+
+# city = input()
+# product = input()
+#
+# if city == 'S':
+#     if product == 'C':
+#         product_price = 13.00
+#     elif..
+#     elif..
+#
+# elif city == 'P':
+#     if product == 'C':
+#         product_price = 13.00
+#     elif..
+#     elif..
+#
+# elif city == 'V':
+#     if product == 'C':
+#         product_price = 13.00
+#     elif..
+#     elif..
+#
+# print('Hello')
+#
+# print = '13'
+#
+# print('Hello')  # '13'('Hello')
+
+# print(True)  # keyword
+#
+# True = 13
+#
+# and
+# or
+# not
+
+# num = 101
+#
+# # print(num > 50)  # True
+# # print(num % 2 == 0)  # True
+# #
+# if num > 50 and num % 2 == 0:
+#     print('it is greater than 50 and even')
+#
+# # if not num > 50:
+#     print('it is not greater than 50')
+
+# print('Logical AND:')
+# print(f'T and T == {True and True}')
+# print(f'T and F == {True and False}')
+# print(f'F and T == {False and True}')
+# print(f'F and F == {False and False}')
+#
+# print('----')
+# print('Logical OR:')
+# print(f'T or T == {True or True}')
+# print(f'T or F == {True or False}')
+# print(f'F or T == {False or True}')
+# print(f'F or F == {False or False}')
+#
+# print('---')
+# print('Logical NOT')
+# print(f'NOT T == {not True}')
+# print(f'NOT F == {not False}')
+#
+# num = 10
+# if not num > 50:
+#     ...
+
+# if not is_superadmin or not is_admin:
+#     log_as_user()
+
+# print(True or (False and False))     # and has precedence
+# print((True or False) and False)   # parentheses change precedence
+#
+# print(not False or True)        # not has precedence
+# print(not (False or True))      # parentheses change precedence
+
+# import this
+
+# num = 100
+#
+# if num > 50 and num % 2 == 0:
+#         print('yes')
+
+# number = int(input())
+#
+# # if -100 <= number <= 100 and number != 0:
+# if -100 <= number <= 100 and number != 0:
+#     print("Yes")
+# else:
+#     print("No")
+
+# name = 'Simo'
+#
+# if name == 'Vladi' or name == 'Georgi':
+#     print('I am Vladi or Georgi')
+# else:
+#     print('😁')
+#
