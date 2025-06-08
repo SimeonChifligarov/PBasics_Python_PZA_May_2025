cda2a62dd75e597fb5751a0c49f429ee55d4d40d
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-08 09:26:58 +0300

    add 03_01_Conditional_Statements_Advanced_Lab 09_Fruit_or_Vegetable.py

---
+++ 03_Conditional_Statements_Advanced/03_01_Conditional_Statements_Advanced_Lab/09_Fruit_or_Vegetable.py
@@ -0,0 +1,14 @@
+# •	Плодовете 'fruit' имат следните възможни стойности:  banana, apple, kiwi, cherry, lemon и grapes;
+# •	Зеленчуците 'vegetable' имат следните възможни стойности:  tomato, cucumber, pepper и carrot;
+# •	Всички останали са 'unknown'.
+
+product = input()
+
+if product == 'banana' or product == 'apple' or product == 'kiwi' or product == 'cherry' or product == 'lemon' or product == 'grapes':
+    result = 'fruit'
+elif product == 'tomato' or product == 'cucumber' or product == 'pepper' or product == 'carrot':
+    result = 'vegetable'
+else:
+    result = 'unknown'
+
+print(result)
