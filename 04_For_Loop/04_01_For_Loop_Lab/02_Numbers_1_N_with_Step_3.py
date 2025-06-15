da85d450d852edbca869aabbf42c9f2f4df92985
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-15 08:58:19 +0300

    add 02_Numbers_1_N_with_Step_3.py

---
+++ 04_For_Loop/04_01_For_Loop_Lab/02_Numbers_1_N_with_Step_3.py
@@ -0,0 +1,12 @@
+upper_bound = int(input())
+
+for num in range(1, upper_bound + 1, 3):
+    print(num)
+
+# user_input = input()
+# #
+# for ch in user_input:
+#     print(ch)
+#
+# print('My', 'name', 'is', 'Simo', sep='😎😁', end='###')
+# print('!', end='\n')
