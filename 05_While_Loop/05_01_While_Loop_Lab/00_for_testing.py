2f55c9f0e7910a4848e0cdbb0ac337841056acdf
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-21 13:33:49 +0300

    add 00_for_testing.py

---
+++ 05_While_Loop/05_01_While_Loop_Lab/00_for_testing.py
@@ -0,0 +1,88 @@
+# for i in range(5):
+#     if i == 2:
+#         continue
+#
+#     print(i)
+
+
+# 0, 1, 3, 4
+
+# [pseudocode]
+#
+# for st in students_PZA_2025:
+#     send_email(st.exam_result)
+#
+#     if st.exam_result >= 5.00:
+#         print_certificate(st)
+
+# for i in range(5, 0):
+#     print(i)
+
+# for i in range(97, 100):
+#     print(chr(i))  # print(chr(97))
+
+
+# print(chr(97))
+# print(ord('a'))
+
+# for i in range(0, 4, 1):
+#     print(i / 2)
+
+# import time
+# number = 0.0
+#
+# while number < 2:
+#     time.sleep(1)
+#     print(number)
+    # number += 0.5
+
+# name = input()
+#
+# while name != 'admin':
+#     if name == 'Nakov':
+#         print('will exit!')
+#         name = input()
+#         continue
+#
+#     print(f'h3ll0, {name}!!!')
+#
+#     name = input()
+
+# while True:
+#     name = input()
+#
+#     if name == 'admin':
+#         continue
+#
+#     if name == 'Nakov':
+#         print('will exit')
+#         break
+#
+#     print(f'Hello {name}!!!')
+
+# import time
+# number = 0.0
+#
+# while number < 2:
+#     time.sleep(1)
+#     print(number)
+#     number += 0.5
+
+# a = 5
+#
+# while a <= 7:
+#     print("a = " + str(a))
+#     a += 1
+#
+# print('---')
+# print(a)
+
+num1 = 0.1
+num2 = 0.2
+
+num3 = num1 + num2
+
+print(num3)  # 0.3
+
+
+
