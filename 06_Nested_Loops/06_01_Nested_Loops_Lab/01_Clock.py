81a510cfbacef1c7904d5be894ed35bf370c10d4
SimeonChifligarov <simeon.chifligarov@gmail.com>
2025-06-28 13:33:50 +0300

    add 01_Clock.py

---
+++ 06_Nested_Loops/06_01_Nested_Loops_Lab/01_Clock.py
@@ -0,0 +1,10 @@
+# import time
+
+for hour in range(24):  # [0, 1, ... 23]
+    # print(hour)
+
+    for minute in range(60):  # [0, 1, ... 59]
+        # print(minute)
+        # time.sleep(1)
+        # print(f'\r{hour}:{minute}', end='')
+        print(f'{hour}:{minute}')
